889964870cef5a52da632b986298d6c2cd443c08
Agustin <agusdantuoni25@gmail.com>
2021-02-28 13:54:08 -0300

    Initial commit

---
+++ lib/utils/globals.dart
@@ -0,0 +1,8 @@
+library my_prj.globals;
+
+// Testing Only (No other credentials shared cause its only an example to help other developers to set up the checkout)
+String mpPublicKey = "TEST-25199daf-70b4-4824-ab7a-1d04e59f7b70";
+String accesstoken =
+    "TEST-1688968425805611-101303-98836ff9bc436a293fb7270754214747-398509220";
+String clientId = "1688968425805611";
+String clientSecret = "Fkq1n1Dv1hfgr68sh7nTwY2sdG3xFsOz";
